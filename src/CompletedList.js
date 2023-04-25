228b5afeb12ef12e0d4ded33e8afd7b8ae190e7e
Surya Teja <pratapreddy0808@gmail.com>
2023-04-25 15:30:29 +0530

    Added Completed List

--- src/CompletedList.js
+++ src/CompletedList.js
@@ -1,12 +1,6 @@
 import Todo from "./Todo";
-import { RxUpdate } from 'react-icons/rx'
-import { GiCancel } from 'react-icons/gi'
-import { useRef } from "react";
 
 export default function CompletedList( { todos , toggleTodo , editTodo , updateTodo , cancelUpdate , deleteTodo }) {
-  const title = useRef()
-  const dueDate = useRef()
-
   return ( 
     todos.map(todo => {
       
